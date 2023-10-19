91e9a84a19c67837e19d7de3f00d963e76e69882
DungNT <dungnthe176732@fpt.edu.vn>
2023-10-19 18:02:17 +0700

    update bot.properties

--- src/main/java/com/discordbot/mathew/MathewApplication.java
+++ src/main/java/com/discordbot/mathew/MathewApplication.java
@@ -2,8 +2,10 @@ package com.discordbot.mathew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:bot.properties")
 public class MathewApplication {
 
 	public static void main(String[] args) {
